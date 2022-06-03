35547d4c5c8a2baf5354d71066a7a51be42428f9
JandroSC <salgadojandro@gmail.com>
2022-06-03 13:39:42 +0200

    [MOD] Pantalla de Registro

---
+++ RaggzEzkape/Assets/Scripts/PlayFabRegister.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFabRegister : MonoBehaviour
+{
+
+    public void test()
+    {
+        Debug.Log("click!");
+    }
+
+
+}
